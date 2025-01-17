56ffa162ada2e393675ce4311cbbab165d7c2518
arthureastwood <arthurpenragon@gmail.com>
2025-01-17 05:54:59 -0800

    Function: playRound
    Enables the human and computer to play one round

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-function getComputerChoice(cc) {
+function getComputerChoice() {
     const cc = Math.floor(Math.random() * 3) + 1;
     if(cc === 1){
         return "rock";
@@ -13,7 +13,7 @@ function getComputerChoice(cc) {
     console.log(cc);  
 }
 
-function getHumanChoice(hc){
+function getHumanChoice(){
     const hc = prompt("The user can now play", '');
     if(hc === "rock"){
         return "rock";
@@ -26,3 +26,40 @@ function getHumanChoice(hc){
     }
     console.log(hc);
 }
+
+function playRound(humanSelection, computerSelection){
+    if(humanSelection === computerSelection){
+        console.log(`It's a tie! you both picked ${humanSelection}`);
+    }
+    else if(humanSelection === "rock" && computerSelection === "scissors"){
+        console.log("You Win! Rock beats Scissors");
+        humanScore++;
+    }
+    else if(humanSelection === "paper" && computerSelection === "rock"){
+        console.log("You Win! Paper beats Rock");
+        humanScore++;
+    }
+    else if(humanSelection === "scissors" && computerSelection === "paper"){
+        console.log("You Win! Scissors beats Paper");
+        humanScore++;
+    }
+    else{
+        console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+        computerScore++;
+    }
+    if(humanScore === computerScore){
+        console.log("The Human and Computer tie");
+    }
+    else if(humanScore>computerScore){
+        console.log(`Human: ${humanScore} beat the Computer: ${computerScore} in this round.`);
+    }
+    else if(humanScore<computerScore){
+        console.log(`Human: ${humanScore} loses to the Computer: ${computerScore} in this round.`);
+    }
+    console.log(`Human: ${humanScore}`);
+    console.log(`Computer: ${computerScore}`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
