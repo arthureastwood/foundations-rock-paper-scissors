bfe1cbab8ae41bf818d14573ef1c81ab6316839c
arthureastwood <arthurpenragon@gmail.com>
2025-02-17 11:37:01 +0300

    Added a UI to the RPS Game
    A UI makes it easy for users to play the game instead of using the console.

--- script.js
+++ script.js
@@ -1,81 +1,118 @@
-const options = ["rock", "paper", "scissors"];
+// Project: Rock Paper Scissors
 
+// rock button
+const rockBtn = document.querySelector("#rock");
+rockBtn.classList.add("playButtons");
+
+// paper button
+const paperBtn = document.querySelector("#paper");
+paperBtn.classList.add("playButtons");
+// scissors button
+const scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.classList.add("playButtons");
+
+// new game button
+const newGameBtn = document.querySelector("#newGame");
+newGameBtn.classList.add("newGameButton");
+
+const roundScore = document.querySelector("#roundScore");
+const finalScore = document.querySelector("#finalScore");
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
+
+// function randomly returns "Rock", "Paper", or "Scissors"
 function getComputerChoice() {
-    const computerChoice = options[Math.floor(Math.random() * options.length)];
-    return computerChoice;
-    
+  let options = { 1: "rock", 2: "paper", 3: "scissors" };
+
+  return options[Math.floor(Math.random() * 3) + 1];
 }
 
-function getHumanChoice(){
-    let validatedInput = false;
-    while(validatedInput===false){
-        const choice = prompt("Rock Paper Scissors");
-        if(choice===null){
-            continue;
-        }
-        const choiceInLowerCase = choice.toLowerCase();
-        if(options.includes(choiceInLowerCase)){
-            validatedInput = true;
-            return choiceInLowerCase;
-        }
-    }
-    
+// set initial values for the game
+let playerScore = 0;
+let computerScore = 0;
+let isGameOver = false;
+let scoreToReach = 5;
+
+// each button calls playRound when clicked
+// each button can call endGame when scoreToReach = 5
+rockBtn.addEventListener("click", function () {
+  if (!isGameOver) {
+    playRound("rock", getComputerChoice());
+    endGame();
+  }
+});
+
+paperBtn.addEventListener("click", function () {
+  if (!isGameOver) {
+    playRound("paper", getComputerChoice());
+    endGame();
+  }
+});
+
+scissorsBtn.addEventListener("click", function () {
+  if (!isGameOver) {
+    playRound("scissors", getComputerChoice());
+    endGame();
+  }
+});
+
+// restart the game by reseting score, clearing score log, and removing classes
+
+newGameBtn.addEventListener("click", function () {
+  isGameOver = false;
+  playerScore = 0;
+  computerScore = 0;
+  roundScore.innerText = "";
+  player.innerText = "0";
+  computer.innerText = "0";
+  finalScore.innerText = "";
+  player.classList.remove("winner", "loser");
+  computer.classList.remove("winner", "loser");
+});
+
+// for each round, a new li is added as a round score log
+// player or computer score increments
+// player and computer score displays each round
+function playRound(playerSelection, computerSelection) {
+  const newLi = document.createElement("li");
+  newLi.classList.add("newLi");
+  if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors")
+  ) {
+    newLi.innerText = `You chose ${playerSelection} and the computer chose ${computerSelection}. You lost.`;
+    roundScore.appendChild(newLi);
+    computerScore++;
+    computer.innerText = computerScore;
+  } else if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    newLi.innerText = `You chose ${playerSelection} and the computer chose ${computerSelection}. You win!`;
+    roundScore.appendChild(newLi);
+    playerScore++;
+    player.innerText = playerScore;
+  } else if (playerSelection === computerSelection) {
+    newLi.innerText = `You chose ${playerSelection} and the computer chose ${computerSelection}. That's a tie!`;
+    roundScore.appendChild(newLi);
+  }
 }
 
-function checkWinner(humanSelection, computerSelection){
-    if(humanSelection === computerSelection){
-        return "Tie";
+// winner and loser delcared when player or computer score equals scoreToReach
+// styles added for winner and loser
+function endGame() {
+  if (playerScore === scoreToReach || computerScore === scoreToReach) {
+    isGameOver = true;
+    if (playerScore === scoreToReach) {
+      finalScore.innerText = "You won the game!";
+      player.classList.add("winner");
+      computer.classList.add("loser");
+    } else if (computerScore === scoreToReach) {
+      finalScore.innerText = "Computer wins the game!";
+      computer.classList.add("winner");
+      player.classList.add("loser");
     }
-    else if(
-        (humanSelection==="rock" && computerSelection==="scissors") ||
-        (humanSelection==="scissors" && computerSelection==="paper") ||
-        (humanSelection==="paper" && computerSelection==="rock")
-    ){
-        return "Player";
-    }
-    else{
-        return "Computer";
-    }
+  }
 }
-
-function playRound(humanSelection, computerSelection){
-    const result = checkWinner(humanSelection, computerSelection);
-    if(result === "Tie"){
-        return `It's a tie! you both picked ${humanSelection}`;
-    }
-    else if(result === "Player"){
-        return `You Win! ${humanSelection} beats ${computerSelection}`;
-    }
-    else{
-        return `You lose! ${computerSelection} beats ${humanSelection}`;
-    }
-}
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    for(i=0; i<5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
-        console.log("--------------------------------------------");
-        if(checkWinner(humanSelection, computerSelection) ==="Player"){
-            humanScore++;
-        }
-        else if(checkWinner(humanSelection, computerSelection) ==="Computer"){
-            computerScore++;
-        }
-    }
-    console.log("Game Over!");
-    if(humanScore>computerScore){
-        console.log("Player wins!");
-    }
-    else if(humanScore<computerScore){
-        console.log("Computer wins!");
-    }
-    else{
-        console.log("It is a tie!");
-    }
-
-}
-
